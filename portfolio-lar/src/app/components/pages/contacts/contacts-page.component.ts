04501ed18d9b371e1437449b1ca0b18e7509cc88
coolinich <coolinich@gmail.com>
2021-05-07 15:43:40 +0300

    added telegram bot for contacts, fix for menu closing

--- portfolio-lar/src/app/components/pages/contacts/contacts-page.component.ts
+++ portfolio-lar/src/app/components/pages/contacts/contacts-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MailService } from 'src/app/services/mail.service';
+import { ContactBotService } from 'src/app/services/contact-bot.service';
+import { BotMessageBody } from 'src/app/interfaces/bot';
+import { CHAT_ID } from 'src/environments/environment';
 
 @Component({
   selector: 'pkl-contacts-page',
@@ -21,7 +24,8 @@ export class ContactsPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private mailService: MailService
+    private mailService: MailService,
+    private contactBotService: ContactBotService
   ) {
     this.contactForm = fb.group({
       'contactFormName': ['', Validators.required],
@@ -39,7 +43,7 @@ export class ContactsPageComponent implements OnInit {
     return this.contactForm.get('contactFormCopy') as FormGroup;
   }
 
-  onSubmit() {
+  onSubmitEmail() {
     this.mailService.sendEmail(this.contactForm.value).subscribe(
       (response) => {
         console.log('success from component', response);
@@ -47,7 +51,29 @@ export class ContactsPageComponent implements OnInit {
         this.disabledSubmitButton = true;
       },
       (error) => {
-        console.log('error from component', error);
+        console.error('error from component', error);
+      }
+    )
+  }
+
+  onSubmitToBot() {
+    const botMessage: BotMessageBody = {
+      chat_id:  CHAT_ID,
+      text: `
+      <b>Name:</b> ${this.contactForm.value?.contactFormName};
+      <b>Email:</b> ${this.contactForm.value?.contactFormEmail};
+      <b>Text:</b> ${this.contactForm.value?.contactFormMessage};
+      `,
+      parse_mode: 'HTML'
+    } 
+    this.contactBotService.sendMessageToBot(botMessage).subscribe(
+      (response) => {
+        console.info('Success when contact bot', response);
+        this.contactForm.reset();
+        this.disabledSubmitButton = true;  
+      },
+      (error) => {
+        console.error('Error when contact bot', error)
       }
     )
   }
